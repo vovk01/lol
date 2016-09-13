44b43e5f16aa106212e5c9f067b900182dae8cce
Oktagram <oktagram98@gmail.com>
2016-09-13 21:02:28 +0300

    Anton 3d commit

--- New_project/Form1.cs
+++ New_project/Form1.cs
@@ -5,34 +5,14 @@ namespace New_project
 {
     public partial class Form1 : Form
     {
-	    private Label[] _playerFields;
-	    private const int CountOfPlayerFields = 37;
-
         public Form1()
         {
             InitializeComponent();
-			InitializeLabelArray();
 			FillAreaWithLabels();
         }
 
-	    private void InitializeLabelArray()
-	    {
-			_playerFields = new Label[CountOfPlayerFields];
-
-		    for (var i = 0; i < CountOfPlayerFields; i++)
-		    {
-			    _playerFields[i] = new Label()
-			    {
-				    Dock = DockStyle.Fill,
-					BackColor = Color.Black
-			    };
-		    }
-	    }
-
 	    private void FillAreaWithLabels()
 	    {
-		    var index = 0;
-
 		    int[] countForPlacement = { 3, 5, 7, 7, 7, 5, 3 };
 			int[] valuesForPlacement = { 5, 3, 1, 1, 1, 3, 5 };
 
@@ -40,7 +20,15 @@ namespace New_project
 			{
 				for (var j = 0; j < countForPlacement[i]; j++)
 				{
-					Panel.Controls.Add(_playerFields[index++], i * 2 + 1, j * 2 + valuesForPlacement[i]);
+					Panel.Controls.Add(
+						new Label
+						{
+							Dock = DockStyle.Fill,
+							BackColor = Color.Black
+						}, 
+						i * 2 + 1, 
+						j * 2 + valuesForPlacement[i]
+					);
 				}
 			}
 	    }
